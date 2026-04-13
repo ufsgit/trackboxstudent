0f39ccb681dad372a0490a241c26e96e56f873d0
Minhaj Muhammad <minhajmuhammad259@gmail.com>
2026-04-13 17:34:12 +0530

    build

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -13,6 +13,31 @@ subprojects {
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
 subprojects {
+    val configureAndroid: (Project) -> Unit = { p ->
+        if (p.extensions.findByName("android") != null) {
+            val android = p.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            android.apply {
+                compileSdkVersion(36)
+                defaultConfig {
+                    targetSdkVersion = 34
+                }
+                if (namespace == null) {
+                    namespace = p.group.toString()
+                }
+            }
+        }
+    }
+
+    if (project.state.executed) {
+        configureAndroid(project)
+    } else {
+        project.afterEvaluate {
+            configureAndroid(this)
+        }
+    }
+}
+
+subprojects {
     project.evaluationDependsOn(":app")
 }
 
