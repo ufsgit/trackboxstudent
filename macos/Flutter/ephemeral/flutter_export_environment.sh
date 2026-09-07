9f628718f226658831fe804e00fa083343ff5f36
Nihal Office <ufstechgithub@gmail.com>
2026-09-07 17:05:02 +0530

    feat: configure Android build environment and integrate Firebase services

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/ufstechnologies/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/ufstechnologies/Desktop/trackboxstudent"
+export "FLUTTER_ROOT=D:\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\trackboxstudent"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0"
-export "FLUTTER_BUILD_NUMBER=17"
+export "FLUTTER_BUILD_NAME=1.0.3"
+export "FLUTTER_BUILD_NUMBER=30"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
